ded682b4f8b0c2f35c1f79bd76972dd6d503236b
Firdavs Nematov <118260604+Firdavs-Nematov@users.noreply.github.com>
2023-02-05 15:39:07 +0500

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,10 @@
+let options = {
+    strings: ["Front End Developer", "Back end Developer"],
+    typeSpeed: 80,
+    backSpeed:80,
+    loop: true,
+    loopCount: Infinity
+
+};
+
+let typed = new Typed('.element', options);
